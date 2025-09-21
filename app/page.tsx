1d315f54646f84ad6f8b357d429c4dd8b9e041aa
RoyvSichem <royvs@live.nl>
2025-09-21 16:21:20 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,15 @@
+'use client';
+import { useEffect } from 'react';
+
+export default function CodeBridge(){
+  useEffect(()=>{
+    const p = new URLSearchParams(location.search);
+    const code = p.get('code');
+    if(code) location.href = `/auth/callback?code=${code}`;
+  },[]);
+  return null;
+}
+
 import { supabaseServer } from '../lib/supabase';
 
 export const dynamic = 'force-dynamic';
