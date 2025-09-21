1a9d7e336f15ce115b1758c4438d833de2b10ec2
RoyvSichem <royvs@live.nl>
2025-09-21 15:14:29 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import './globals.css';
+
 export const metadata = { title: 'Awaren', description: 'Meditaties en meer' };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
